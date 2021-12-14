e4d7d60a37d094d67db8d6325809f092142acd14
braziltaiany <braziltaiany@outlook.com>
2021-12-14 12:03:25 -0300

    adicionando projeto de média de quilometragem por litro

---
+++ Km/Km/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace Km
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textPainel = new System.Windows.Forms.TextBox();
+            this.textViagem = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textLitros = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textPainel
+            // 
+            this.textPainel.Location = new System.Drawing.Point(36, 45);
+            this.textPainel.Name = "textPainel";
+            this.textPainel.Size = new System.Drawing.Size(268, 23);
+            this.textPainel.TabIndex = 0;
+            // 
+            // textViagem
+            // 
+            this.textViagem.Location = new System.Drawing.Point(36, 112);
+            this.textViagem.Name = "textViagem";
+            this.textViagem.Size = new System.Drawing.Size(268, 23);
+            this.textViagem.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(36, 224);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(268, 23);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Valor painel do carro(km):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Valor final da viagem(km):";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(125, 262);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(87, 15);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "média de km/L";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Litros abastecidos:";
+            // 
+            // textLitros
+            // 
+            this.textLitros.Location = new System.Drawing.Point(36, 179);
+            this.textLitros.Name = "textLitros";
+            this.textLitros.Size = new System.Drawing.Size(268, 23);
+            this.textLitros.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 294);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textLitros);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.textViagem);
+            this.Controls.Add(this.textPainel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textPainel;
+        private System.Windows.Forms.TextBox textViagem;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textLitros;
+    }
+}
+
---
+++ Km/Km/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Km
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double painel, viagem, total, media, litros;
+
+            painel = Convert.ToDouble(textPainel.Text);
+            viagem = Convert.ToDouble(textViagem.Text);
+            litros = Convert.ToDouble(textLitros.Text);
+
+
+            total = viagem - painel;
+            media = total / litros;
+
+            lblResultado.Text = media.ToString("F2");
+        }
+    }
+}
