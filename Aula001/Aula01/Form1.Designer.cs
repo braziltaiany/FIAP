d06bb71515446fc9afc11f3e1d67ce7c39d23fdd
braziltaiany <braziltaiany@outlook.com>
2021-12-16 19:10:25 -0300

    Organizando projeto

---
+++ Aula001/Aula01/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Aula01
+{
+    partial class frmAula01
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBoasVindas = new System.Windows.Forms.Button();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.textNome = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBoasVindas
+            // 
+            this.btnBoasVindas.Location = new System.Drawing.Point(88, 83);
+            this.btnBoasVindas.Name = "btnBoasVindas";
+            this.btnBoasVindas.Size = new System.Drawing.Size(75, 23);
+            this.btnBoasVindas.TabIndex = 0;
+            this.btnBoasVindas.Text = "Clique aqui";
+            this.btnBoasVindas.UseVisualStyleBackColor = true;
+            this.btnBoasVindas.Click += new System.EventHandler(this.btnBoasVindas_Click);
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Location = new System.Drawing.Point(13, 146);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(0, 15);
+            this.lblMensagem.TabIndex = 1;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(13, 13);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(106, 15);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "Digite o seu nome:";
+            // 
+            // textNome
+            // 
+            this.textNome.Location = new System.Drawing.Point(13, 31);
+            this.textNome.Name = "textNome";
+            this.textNome.Size = new System.Drawing.Size(234, 23);
+            this.textNome.TabIndex = 3;
+            // 
+            // frmAula01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 232);
+            this.Controls.Add(this.textNome);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.btnBoasVindas);
+            this.Name = "frmAula01";
+            this.Text = "Aula 01";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBoasVindas;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox textNome;
+    }
+}
+
---
+++ Aula001/Aula01/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula01
+{
+    public partial class frmAula01 : Form
+    {
+        public frmAula01()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBoasVindas_Click(object sender, EventArgs e)
+        {
+            string nomeUsuario;// Declaração de variavel do tipo string
+
+            nomeUsuario = textNome.Text; // coloca nome do usuario dentro da variavel
+
+            MessageBox.Show("Bem vindo ao C# " + nomeUsuario, "Olá", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            lblMensagem.Text = "Bem vindo ao C# " + nomeUsuario;
+            
+            //MessageBox.Show($"Bem vindo ao C#  {nomeUsuario}", "Olá", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //lblMensagem.Text = $"Bem vindo ao C#  {nomeUsuario}";
+
+
+        }
+    }
+}
