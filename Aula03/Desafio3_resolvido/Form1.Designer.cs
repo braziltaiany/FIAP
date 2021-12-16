4ce81c17a33de43290e0abfa8e9cc3b001161ad7
braziltaiany <braziltaiany@outlook.com>
2021-12-16 13:44:37 -0300

    adicionando desafio resolvido pelo professor

---
+++ Aula03/Desafio3_resolvido/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Desafio3_resolvido
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbOpcao = new System.Windows.Forms.ComboBox();
+            this.textValor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(12, 131);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(179, 23);
+            this.btnConfirmar.TabIndex = 9;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Digite o valor: ";
+            // 
+            // cmbOpcao
+            // 
+            this.cmbOpcao.FormattingEnabled = true;
+            this.cmbOpcao.Items.AddRange(new object[] {
+            "Depósito",
+            "Saque",
+            "Ver saldo"});
+            this.cmbOpcao.Location = new System.Drawing.Point(15, 86);
+            this.cmbOpcao.Name = "cmbOpcao";
+            this.cmbOpcao.Size = new System.Drawing.Size(179, 23);
+            this.cmbOpcao.TabIndex = 6;
+            // 
+            // textValor
+            // 
+            this.textValor.Location = new System.Drawing.Point(15, 44);
+            this.textValor.Name = "textValor";
+            this.textValor.Size = new System.Drawing.Size(179, 23);
+            this.textValor.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(206, 182);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbOpcao);
+            this.Controls.Add(this.textValor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbOpcao;
+        private System.Windows.Forms.TextBox textValor;
+    }
+}
+
---
+++ Aula03/Desafio3_resolvido/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desafio3_resolvido
+{
+    public partial class Form1 : Form
+    {
+        double saldo = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            double valor;
+
+            //valor = double.Parse(textValor.Text); ou com tratamento de erro
+            if(double.TryParse(textValor.Text, out valor) == false)
+            {
+                MessageBox.Show("");
+                textValor.Clear();
+                textValor.Focus();
+                return;
+            }
+
+            switch (cmbOpcao.SelectedIndex)
+            {
+                case 0:
+                    saldo = saldo + valor;
+                    MessageBox.Show("Depósito efetuado!");
+                    break;
+                case 1:
+                    saldo = saldo - valor;
+                    MessageBox.Show("Saque efetuado!");
+                    break;
+                case 2:                 
+                    MessageBox.Show($"Seu saldo é de R${saldo.ToString("F2")}");
+                    break;
+                default:                    
+                    MessageBox.Show("Digite um valor válido!");
+                    break;
+            }
+
+        }
+    }
+}
