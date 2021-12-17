f4e2502064ebb02b0acbd5033e84f49ac03de16c
braziltaiany <braziltaiany@outlook.com>
2021-12-17 20:20:09 -0300

    Adicionando desafio 2 de matriz

---
+++ Aula05/MatrizDesafio2_SomaMediaMultip/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace MatrizDesafio2_SomaMediaMultip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMult = new System.Windows.Forms.Button();
+            this.btnMedia = new System.Windows.Forms.Button();
+            this.btnSoma = new System.Windows.Forms.Button();
+            this.lstResultado = new System.Windows.Forms.ListBox();
+            this.btnMatriz = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMult
+            // 
+            this.btnMult.Location = new System.Drawing.Point(37, 109);
+            this.btnMult.Name = "btnMult";
+            this.btnMult.Size = new System.Drawing.Size(246, 23);
+            this.btnMult.TabIndex = 5;
+            this.btnMult.Text = "Multiplicar elemento linha 3";
+            this.btnMult.UseVisualStyleBackColor = true;
+            this.btnMult.Click += new System.EventHandler(this.btnMult_Click);
+            // 
+            // btnMedia
+            // 
+            this.btnMedia.Location = new System.Drawing.Point(37, 79);
+            this.btnMedia.Name = "btnMedia";
+            this.btnMedia.Size = new System.Drawing.Size(246, 23);
+            this.btnMedia.TabIndex = 4;
+            this.btnMedia.Text = "Média da linha 2";
+            this.btnMedia.UseVisualStyleBackColor = true;
+            this.btnMedia.Click += new System.EventHandler(this.btnMedia_Click);
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.Location = new System.Drawing.Point(37, 49);
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.Size = new System.Drawing.Size(246, 23);
+            this.btnSoma.TabIndex = 3;
+            this.btnSoma.Text = "Soma linha 1";
+            this.btnSoma.UseVisualStyleBackColor = true;
+            this.btnSoma.Click += new System.EventHandler(this.btnSoma_Click);
+            // 
+            // lstResultado
+            // 
+            this.lstResultado.FormattingEnabled = true;
+            this.lstResultado.Location = new System.Drawing.Point(37, 158);
+            this.lstResultado.Name = "lstResultado";
+            this.lstResultado.Size = new System.Drawing.Size(246, 108);
+            this.lstResultado.TabIndex = 6;
+            // 
+            // btnMatriz
+            // 
+            this.btnMatriz.Location = new System.Drawing.Point(37, 20);
+            this.btnMatriz.Name = "btnMatriz";
+            this.btnMatriz.Size = new System.Drawing.Size(246, 23);
+            this.btnMatriz.TabIndex = 7;
+            this.btnMatriz.Text = "Exibir Matriz";
+            this.btnMatriz.UseVisualStyleBackColor = true;
+            this.btnMatriz.Click += new System.EventHandler(this.btnMatriz_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 307);
+            this.Controls.Add(this.btnMatriz);
+            this.Controls.Add(this.lstResultado);
+            this.Controls.Add(this.btnMult);
+            this.Controls.Add(this.btnMedia);
+            this.Controls.Add(this.btnSoma);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMult;
+        private System.Windows.Forms.Button btnMedia;
+        private System.Windows.Forms.Button btnSoma;
+        private System.Windows.Forms.ListBox lstResultado;
+        private System.Windows.Forms.Button btnMatriz;
+    }
+}
+
---
+++ Aula05/MatrizDesafio2_SomaMediaMultip/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace MatrizDesafio2_SomaMediaMultip
+{
+    public partial class Form1 : Form
+    {
+        int[,] numeros = new int[4, 4];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnMatriz_Click(object sender, EventArgs e)
+        {
+            lstResultado.Items.Clear();
+
+            for (int linha = 0; linha < 4; linha++)
+            {
+                for (int coluna = 0; coluna < 4; coluna++)
+                {
+                    numeros[linha, coluna] = int.Parse(
+                        Interaction.InputBox($"Digite o elemento na posição{linha},{coluna} da matriz"));
+                }
+            }
+
+            for (int linha = 0; linha < 4; linha++) //lenght conta 6
+            {
+                for (int coluna = 0; coluna < 4; coluna++)
+                {
+                    lstResultado.Items.Add($"{numeros[linha, coluna]}");
+                }
+            }
+        }
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+            int soma = 0;
+
+            for (int coluna = 0; coluna < 4; coluna++)
+            {
+                numeros[0, coluna] = int.Parse(
+                    Interaction.InputBox($"Digite o elemento na posição{0},{coluna} da matriz"));
+
+                soma = soma + numeros[0, coluna];
+            }
+
+            MessageBox.Show($"A soma total dos elementos da primeira linha é: {soma}");
+        }
+
+        private void btnMedia_Click(object sender, EventArgs e)
+        {
+            double media, soma = 0;
+
+            lstResultado.Items.Clear();
+
+            for (int coluna = 0; coluna < 4; coluna++)
+            {
+                numeros[1, coluna] = int.Parse(
+                    Interaction.InputBox($"Digite o elemento{1},{coluna} da matriz"));
+                soma = (double)soma + numeros[1, coluna];
+            }
+
+            media = (double)soma / 4.0;
+
+            lstResultado.Items.Add($"Soma total dos elementos da segunda linha é: {soma}");
+            lstResultado.Items.Add($"A media da segunda linha é: {media.ToString("F2")}");
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            lstResultado.Items.Clear();
+
+            int multip, a, b;
+
+            for (int linha = 0; linha < 4; linha++)
+            {
+                for (int coluna = 0; coluna < 4; coluna++)
+                {
+                    numeros[linha, coluna] = int.Parse(
+                    Interaction.InputBox($"Digite o elemento{linha},{coluna} da matriz"));
+                    
+                    if (linha == 2 || linha == 3)
+                    {
+                        a = numeros[2, coluna];
+                        b = numeros[3, coluna];
+
+                        multip = a * b;
+
+                        //lstResultado.Items.Add($"a:{a} e b:{b}");
+
+                        //if (multip != null)
+                        //{
+                            lstResultado.Items.Add($"Multiplicação: {multip}");
+                        //}
+                    }
+                }
+            }
+        }
+    }
+}
