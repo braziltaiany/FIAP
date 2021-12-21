47eaf6b88153fbe063564aae5ad410f46bfb67e7
braziltaiany <braziltaiany@outlook.com>
2021-12-21 09:20:11 -0300

    Adicionando comentários - Correção do professor

--- CsharpDotnet/Aula06_POO/Exercicio1_Vendedor/Vendedor.cs
+++ CsharpDotnet/Aula06_POO/Exercicio1_Vendedor/Vendedor.cs
@@ -2,21 +2,26 @@
 {
     class Vendedor
     {
+        //ATRIBUTOS
         public string nome { get; set; }
         public double salarioBase { get; set; }
 
+        //CONSTRUTORES
+        //Iniciando com variáveis zeradas e sem parametros
         public Vendedor()
         {
             this.nome = "";
             this.salarioBase = 0;
         }
 
+        //Iniciando com parametros
         public Vendedor(string nome, double salarioBase)
         {
             this.nome = nome;
             this.salarioBase = salarioBase;
         }
-
+        
+        //MÉTODOS DA CLASSE
         public double calculoComissao(double valorVendido)
         {
             return valorVendido * 10.00;
