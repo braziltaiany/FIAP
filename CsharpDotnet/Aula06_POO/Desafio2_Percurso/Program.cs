fc508eacd3c088c81c660fb46c0a1b7ca2d45ffe
braziltaiany <braziltaiany@outlook.com>
2021-12-21 10:14:36 -0300

    Adicionando funcionalidade de listar

--- CsharpDotnet/Aula06_POO/Desafio2_Percurso/Program.cs
+++ CsharpDotnet/Aula06_POO/Desafio2_Percurso/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Desafio2_Percurso
+namespace Viagem
 {
     static class Program
     {
